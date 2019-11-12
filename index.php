de02189375ab22c53af19f3b59ca80d60dd34b18
presid <irwinkizz@gmail.com>
2019-11-12 21:12:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+	<title>RealDeal</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/ss.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1,user-scaleable=no">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+	
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light " id="nav">
+	
+		 <a href="/index.php" class="navbar-brand"id="brand">RealDeal</a>
+<button class="navbar-toggle navbar-toggler  navbar-toggler-right" type="button" style="border: 2px solid  #009e47;" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon icon-bar" ></span>
+            </button>
+	<div class="collapse navbar-collapse"  id="navbarNav">
+
+	
+		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Vehicles</a>
+  </li>
+			<li class="nav-item">
+    <a class="nav-link " href="#">Electronics</a>
+     </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Real Estates</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Houses</a>
+		<div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Land</a>
+		<div class="dropdown-divider"></div>
+		<a class="dropdown-item" href="#">Apartments</a></div>
+      
+  </li>
+  
+   <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Fashion</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Men</a>
+		<div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Women</a>
+		<div class="dropdown-divider"></div>
+		<a class="dropdown-item" href="#">Children</a></div>
+      
+  </li>
+			<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Printing</a>
+		<div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Car hire</a>
+		<div class="dropdown-divider"></div>
+		<a class="dropdown-item" href="#">Decorations</a></div>
+      
+  </li>
+	   	<li class="nav-item">
+    <a class="nav-link " href="#">Extras</a>
+     </li>
+			</ul>
+		<form class="form-inline my-2 my-lg-0" id="form">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+	
+	
+
+	
+	</div>
+ </nav>
+
+
+</body>
+	
+</html>
