eba089c45eb48c4c8975df0c44e2c56042419d06
raketa92 <nazar.erdem@gmail.com>
2020-04-05 17:32:43 +0500

    modified layout

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const express = require('express')
+const mysql = require('mysql')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const mainRoutes = require('./routes/main')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
-
-app.listen(PORT, () => {
-    console.log('Server has been started...')
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
-var mysql = require('mysql');
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "nodejs",
-  password: "admin"
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+app.use(mainRoutes)
+
+async function start() {
+  try {
+    const con = mysql.createConnection({
+      host: "localhost",
+      user: "nodejs",
+      password: "admin"
+    });
+
+    con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+
+    app.listen(PORT, () => {
+      console.log('Server has been started...')
+  })
+
+  }catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
