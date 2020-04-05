05d057f58709d6e4c6cd51443dc86cb1914ea7f6
raketa92 <nazar.erdem@gmail.com>
2020-04-05 22:09:08 +0500

    trying mysql queries

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,38 @@
 const {Router} = require('express')
+const mysql = require('mysql')
 const router = Router()
+const mainModel = require('../models/main')
 
 router.get('/', (req, res) => {
-    res.render('index')
+
+    const con = mysql.createConnection({
+        host: "localhost",
+        user: "nodejs",
+        password: "admin",
+        database: 'realdeal'
+      });
+  
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+
+    con.query('SELECT * FROM Product', (err, rows) => {
+        if(err) throw err
+
+        console.log('Data received from Db:');
+        console.log(rows);
+    })
+
+    res.render('index', {
+        title: 'Main page'
+    })
+})
+
+router.get('/signIn', (req, res) => {
+    res.render('signIn', {
+        title: 'Sign in'
+    })
 })
 
 module.exports = router
