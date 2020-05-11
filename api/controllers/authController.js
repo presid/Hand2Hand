663a155a02027a870d027638150ef0ceb3a69e8b
raketa92 <nazar.erdem@gmail.com>
2020-05-11 05:05:43 +0500

    modifying login

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -13,6 +13,13 @@ exports.signIn = async (req, res) => {
 
 exports.signInUser = async (req, res) => {
     try {
+        messages = [];
+        loggedIn = false;
+        if(!req.body.email || !req.body.password) {
+            return res.render('signIn', {
+                message: 'Fill all fields'
+            });
+        }
 
         const user = await User.findOne({
             where: {
@@ -22,16 +29,44 @@ exports.signInUser = async (req, res) => {
 
         // console.log(user);
 
-        if (user == null) { return res.status(401).json({message: 'No user with such email'});}
-        if (user.password.toString() === req.body.password.toString()) {
-            return res.status(200).json({
-                message: `Welcome ${user.user_name}`
-            });
-        } else {
-            res.status(401).json({
-                message: `incorrect password`
-            });
+        if (user == null) { 
+            messages.push('Incorrect email or password'); 
+            loggedIn = false;
+            res.statusCode = 401;
+            return res.render('signIn', {
+                message: messages,
+                loggedIn
+        });
+            // return res.status(401).json({message: 'Incorrect email or password'});
         }
+        if (user.password.toString() === req.body.password.toString()) {
+            messages.push(`welcome ${user.user_name}`);
+            loggedIn = true;
+            return res.render('signIn', {
+                message: messages,
+                loggedIn
+        });
+            // return res.render('signIn', {
+            //     message: `welcome ${user.user_name}`
+            // });
+        } else {
+            messages.push('Incorrect email or password'); 
+            loggedIn = false;
+            res.statusCode = 401;
+            return res.render('signIn', {
+                message: messages,
+                loggedIn
+        });
+            // res.status(401).json({
+            //     message: `Incorrect email or password`
+            // });
+        }
+
+        // return res.render('signIn', {
+        //         message: messages,
+        //         loggedIn
+        // });
+
     } catch (e) {
         console.log(e);
     }
